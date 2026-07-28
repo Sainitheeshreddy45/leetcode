7bceda111f1430e3f31bf250ee0fc1ed2ac72b70
Sainitheesh reddy <sainitheeshreddy45@gmail.com>
2026-07-28 15:02:48 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (62.00%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,15 +1,15 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int counter = 0;
-        for(int i=digits.length-1;i>=0;i--) {
-            int val =  (i==digits.length-1)?1:counter;
-            int sum = digits[i] + val;
-            digits[i] = sum < 9? sum: sum%10;
-            counter = sum/10;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
         }
-        if(counter >0) {
-            return IntStream.concat(IntStream.of(counter), Arrays.stream(digits)).toArray();
-        }
-        return digits;
+
+        int[] result = new int[digits.length + 1];
+        result[0] = 1; 
+        return result;
     }
 }
